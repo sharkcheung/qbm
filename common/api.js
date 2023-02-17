44dcb080187f13a5638f4c95e8fc181f685a886a
zhangsha <zhangsha@qianhaitun.com>
2023-02-17 17:55:16 +0800

    api

--- common/api.js
+++ common/api.js
@@ -1,7 +1,9 @@
 module.exports = (vm) => {
 	const { http } = uni.$u
+	let login = (params, config = {}) => http.post('/Login/index', params, config)
 	let rewardPay = (params, config = {}) => http.post('/reward/pay', params, config)
 	uni.$u.api = {
-		rewardPay
+		login,
+		rewardPay,
 	}
 }
