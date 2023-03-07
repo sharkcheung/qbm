345cdaa059541d05a0de6452f6eb5e0dc5192792
zhangsha@qianhaitun.com <563049899@qq.com>
2023-03-07 23:47:52 +0800

    pay

--- common/config.js
+++ common/config.js
@@ -1,8 +1,15 @@
 module.exports = {
     baseUrl: process.env.NODE_ENV === 'production' ? 'https://console-qbt.qebang.cn/api' : 'http://www.qbtadmin.com/api',
-    staticUrl:  process.env.NODE_ENV === 'production' ? 'https://static.miniapp.qbt.qebang.cn/' : 'http://www.qbtadmin.com',
+	// #ifdef TEST-WEIXIN
+	baseUrl: 'https://test-console-qbt.qebang.cn/api', // 测试环境接口域名
+	// #endif
+    staticUrl:  process.env.NODE_ENV === 'production' ? 'https://static.miniapp.qbt.qebang.cn' : 'http://www.qbtadmin.com',
+	// #ifdef TEST-WEIXIN
+	staticUrl: 'https://test-static.miniapp.qbt.qebang.cn', // 测试环境接口域名
+	// #endif
 	meeting_tmpl_id: 'Nk3i7CsWS-xGXcoo4yWVMEqvHrYrrGjxHJ9_drZiULA',
 	withdraw_tmpl_id: 'pWYHyS7YptDQqfIhAPYh5EjL-7qRfTGCnPjO_Xfcazg',
+	customer_assign_tmpl_id: 'oiRljyTGhNUJIK3yY253aB0dvqXyIn8pJdr5pgqcIMA',
 	tabList: [
 		{
 			name: '首页',
